06eeb42791c958a5a490637b463267737536a23b
L <voxil.com@gmail.com>
2020-10-03 11:22:17 +0300

    Initial commit.

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 a = input ('введите 1е число: ')
 b = input ('введите 2е число: ')
 
-print ('a - b =' + int(a) - int(b))
-print ('a + b =' + int(a) + int(b))
+print ('a - b = ' + str(int(a) - int(b)))
+print ('a + b = ' + str(int(a) + int(b)))
