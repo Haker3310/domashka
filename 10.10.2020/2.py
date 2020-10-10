29e31a62428d35aa4c4d202db3a0e9e46a5d31b2
L <voxil.com@gmail.com>
2020-10-10 11:23:37 +0300

    Initial commit.

--- 10.10.2020/2.py
+++ 10.10.2020/2.py
@@ -1,27 +1,27 @@
 a = int(input('введите номер месяца: '))
 if a == 1:
 	print ('это январь')
-if a == 2:
+elif a == 2:
 	print ('это февраль')
-if a == 3:
+elif a == 3:
 	print ('это март')
-if a == 4:
+elif a == 4:
 	print ('это апрель')
-if a == 5:
+elif a == 5:
 	print ('это май')
-if a == 6:
+elif a == 6:
 	print ('это июнь')
-if a == 7:
+elif a == 7:
 	print ('это июль')
-if a == 8:
+elif a == 8:
 	print ('это август')
-if a == 9:
+elif a == 9:
 	print ('это сентябрь')
-if a == 10:
+elif a == 10:
 	print ('это октябрь')
-if a == 11:
+elif a == 11:
 	print ('это ноябрь')
-if a == 12:
+elif a == 12:
 	print ('это декабрь')
-if a > 12 or a < 1:
+elif a > 12 or a < 1:
 	print ('такого месяца нет)')
