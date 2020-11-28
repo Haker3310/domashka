c3ac9f95faa674746d50e33a40c94f6052abf159
L <voxil.com@gmail.com>
2020-11-28 11:07:30 +0300

    Initial commit.

---
+++ 28.11.2020/_1.py
@@ -0,0 +1,8 @@
+while True:
+	stroka = input('Введите строку : ')
+	while stroka.find('/*') != -1 or stroka.find('*/') != -1:
+		f1= stroka.find('/*')	
+		f2= stroka.find('*/')
+		delete = stroka[f1: f2 + 2]
+		stroka = stroka.replace(delete, '')
+	print (stroka)
